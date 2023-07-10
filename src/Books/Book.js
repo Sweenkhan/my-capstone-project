5e2d717e20f8d7b7cd2d389fcdd1d24514d8840b
sween <khansween@gmail.com>
2023-07-10 23:57:30 +0530

    change

--- src/Books/Book.js
+++ src/Books/Book.js
@@ -15,8 +15,8 @@ function Book() {
         `https://www.googleapis.com/books/v1/volumes?q=flowers&maxResults=40&orderBy=newest&key=${api_key}`
       )
       .then((result) => {
-        console.log(result.data.items)
-        console.log(result.data.items[4].volumeInfo.imageLinks.thumbnail);
+        // console.log(result.data.items[0].volumeInfo.title.length)
+        console.log((result.data.items[5].volumeInfo.title.length > 25) ?  result.data.items[5].volumeInfo.title.slice(0, 25) :  result.data.items[5].volumeInfo.title);
         setBooks(result.data.items);
       });
   }, []);
@@ -27,12 +27,12 @@ function Book() {
         {books.map((book, index) => {
           return (
             <div key={index} className="product"> 
-              <p>{book.volumeInfo.title}</p>
-              {console.log(book.volumeInfo.title.length)}
+              <p>{(book.volumeInfo.title.length > 25) ?  book.volumeInfo.title.slice(0, 25) :  book.volumeInfo.title}</p>
+              {/* {console.log(book.volumeInfo.title.length)} */}
               <h2>{book.volumeInfo.authors[0]}</h2>
 
-              <img src={book.volumeInfo.imageLinks.thumbnail} alt="ab to" />
-              {/* <img src="https://books.google.com/books/content?id=I8OBEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="aaj koi hai" /> */}
+              {/* <img src={book.volumeInfo.imageLinks.thumbnail} alt="ab to" /> */}
+              <img src="https://books.google.com/books/content?id=I8OBEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="aaj koi hai" />
             </div>
           );
         })}
