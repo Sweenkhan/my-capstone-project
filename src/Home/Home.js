277dedaaa59de84b70a9587129f02ade4ba20439
sween <khansween@gmail.com>
2023-07-18 23:56:00 +0530

    added main-img

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react' 
+import MainIng from "../images/home.svg"
  
 import "./Home.css"
 
 function Home() {
   return (
      <div className='home'>
-     <div className='main-bg' >
- 
+     <div className='main-bg' > 
+     <img src={MainIng} alt="main-img" />
      </div>
        
      </div>
