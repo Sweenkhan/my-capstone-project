c3628642945eb1a0228471a632d3dbb188278b7b
sween <khansween@gmail.com>
2023-07-26 20:40:11 +0530

    made some changes in book design

--- src/Books/Book.js
+++ src/Books/Book.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios"; 
 import "./Book.css";
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareIcon from '@mui/icons-material/Share';
  function Book() {
   const [books, setBooks] = useState([]);
  
@@ -26,14 +27,24 @@ import ThumbUpIcon from '@mui/icons-material/ThumbUp';
     <div className="book">
       <div className="products">
         {books.map((book, index) => {
-          return (
-            <div key={index} className="product"> 
+
+          const letter = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d", "e", "f"]; 
+          let generate = "#";
+       
+          for(let i = 0; i < 6; i++){
+             generate += letter[Math.floor(Math.random() * letter.length)]
+          } 
+
+        generate += 10; 
+          return ( 
+            <div key={index} className="product" style={{background: `linear-gradient(${generate}, rgba(0, 0, 0, 0))`}}> 
               <h2>{book.author}</h2>
               <p>{book.title}</p> 
                 
               <img alt="bookImg"  src = {book.image}></img> 
-              <div className="footers">
-                <button className="heartBtn"><ThumbUpIcon sx={{ fontSize: 28 }} className="heartIcon"/></button>
+              <div className="footers" style={{background: `linear-gradient(${generate}, rgba(0, 0, 0, 0))`}}>
+                <button className="heartBtn"><FavoriteBorderIcon sx={{ fontSize: 28 }} className="heartIcon"/></button>
+                <button className="shareBtn"><ShareIcon sx={{ fontSize: 28 }} className="shareIcon"/></button>
               </div>
  
             </div>
