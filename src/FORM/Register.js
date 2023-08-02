5c3e8b500d0b32ed1aff4ce26df357d0c3adae67
sween <khansween@gmail.com>
2023-08-02 23:43:37 +0530

    after user register go to login

--- src/FORM/Register.js
+++ src/FORM/Register.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Register.css"
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+ 
 
 function Register() {
+
+  const navigate = useNavigate()
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  function handleRegister(e){
+    e.preventDefault()
+         axios.post('http://localhost:8080/register', {name, email, phone, username, password})
+         .then((result) =>{
+             if(result.data === "success"){
+                navigate('/login')
+             }
+         })
+  }
+  
+
   return (
     <div className='register'>
     
@@ -10,12 +33,12 @@ function Register() {
     <div className='left'></div>
     <div className='right'> 
       <h2>Register</h2>
-      <form method='post' action='http://localhost:8080/register'>
-        <input type='text' name='name' placeholder='Name'/><br />
-        <input type='email' name='email' placeholder='Email'/><br />
-        <input type='number' name='phone' placeholder='Phone'/><br />
-        <input type='text' name='username' placeholder='Username'/><br />
-        <input type='password' name='password' placeholder='Password'/><br />
+      <form method='post' onSubmit={handleRegister}>
+        <input type='text' name='name' placeholder='Name' value={name} onChange={e => setName(e.target.value)} /><br />
+        <input type='email' name='email' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} /><br />
+        <input type='number' name='phone' placeholder='Phone' value={phone} onChange={e => setPhone(e.target.value)} /><br />
+        <input type='text' name='username' placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} /><br />
+        <input type='password' name='password' placeholder='Password'value={password} onChange={e => setPassword(e.target.value)} /><br />
         <div className='btns'> <button type='submit' name='register'>Register</button>
         <Link to="/login">Login</Link>
         </div>
