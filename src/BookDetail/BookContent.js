3c1988c977db02520746b60200407c82baf472cc
sween <khansween@gmail.com>
2023-08-17 20:58:04 +0530

    new para add

--- src/BookDetail/BookContent.js
+++ src/BookDetail/BookContent.js
@@ -4,21 +4,18 @@ with a chorus of voices that where the sun kissed the emerald valleys, a tale of
 
 High atop the cliffs, a lighthouse stood sentinel, its beacon guiding lost souls home.  In the distant land of Veridonia, where the sun kissed the emerald valleys, a tale of magic and mystery unfurled. The ancient oak trees whispered secrets to the wind, their leaves dancing in a graceful waltz. As the moon cast its silvery glow upon the tranquil waters, a lone figure emerged from the shadows. The scent of wildflowers hung in the air, weaving a tapestry of fragrant dreams. Amidst the bustling market, vendors peddled their wares
 
-he cobbled streets. High atop the cliffs, a lighthouse stood sentinel, its beacon guiding lost souls home,
+with a chorus of vohe cobbled streets.`;
 
- 
- `;
 
 const paraThree = `Through the pages of time, a forgotten legend began to resurface, like ink spreading through parchment. Beneath the star-studded canopy, a campfire crackled, casting dancing shadows upon the faces of weary travelers. The castle walls stood tall and proud, a testament to the strength and resilience of a bygone era. In the heart of the enchanted forest, magical creatures frolicked in harmony, their laughter a symphony of joy. Across the rolling hills, a river meandered, its waters teeming with secrets yet to be discovered.
 
 A lone ship sailed toward the horizon, its sails billowing like the wings of a majestic bird. With each brushstroke, the artist breathed life into the canvas, creating a masterpiece that would endure through time. In the quiet village, children chased butterflies, their laughter a melody that carried on the breeze. The aroma of freshly baked bread wafted from the bakery, tempting even the most disciplined of souls. Hidden deep within the labyrinthine catacombs, an ancient artifact lay dormant, waiting for the chosen one to awaken its power.
 
-As the sun dipped below the horizon, a symphony of colors painted the sky, leaving all who beheld it in awe. Among the pages of an old tome, a riddle awaited, promising untold riches to those who could decipher its code. Through the twisted alleyways, a rogue navigated with practiced ease, his cloak billowing behind him like a shadowy specter. The scent of rain hung heavy in the air, a promise of renewal and growth. At the crossroads of destiny, two souls collided, setting in motion a chain of events that would shape their fates forever.
-
-Within the depths of the crystal cave, a rare gemstone shimmered, its beauty a reflection of the earth's secrets. Among the ruins of an ancient civilization, a lone archaeologist pieced together the puzzle of a lost civilization. The wind whispered ancient songs to the weary wanderer, carrying tales of far-off lands and forgotten heroes. In the heart of the bustling city, a street performer captivated a crowd with his mesmerizing dance. The tapestries adorning the palace walls told stories of battles won, alliances forged, and love that transcended time.
+As the sun dipped below the horizon, a symphony of colors painted the sky, leaving all who beheld it in awe. Among the pages of an old tome,  carrying tales of far-off lands and forgotten heroes. In the heart of the bustling city, a street performer captivated a crowd with his mesmerizing dance. The tapestries adorning the palace walls told stories of battles won, alliances forged, and love that transcended time.
 ` 
 
-const paraFour = ` a vintner toiled, crafting wines that would tell stories of sun-soaked fields and rich soil. The flickering candlelight cast eerie shadows upon the ancient manuscripts, where forgotten knowledge lay dormant. Within the opulent palace, a masquerade ball unfolded, the guests' elaborate masks concealing their true identities. Across the celestial expanse, shooting stars streaked, carrying wishes to distant corners of the universe. In the heart of a bustling kitchen, a chef orchestrated a symphony of flavors, each dish a work of culinary art.
+
+const paraFour = ` A vintner toiled, crafting wines that would tell stories of sun-soaked fields and rich soil. The flickering candlelight cast eerie shadows upon the ancient manuscripts, where forgotten knowledge lay dormant. Within the opulent palace, a masquerade ball unfolded, the guests' elaborate masks concealing their true identities. Across the celestial expanse, shooting stars streaked, carrying wishes to distant corners of the universe. In the heart of a bustling kitchen, a chef orchestrated a symphony of flavors, each dish a work of culinary art.
 
 The ancient standing stones whispered secrets to those who listened, their enigmatic messages a puzzle to be deciphered. A nomadic tribe traversed the endless dunes, their camels carrying tales of a life lived in harmony with the desert. The scent of pine and woodsmoke hung in the air, a comforting embrace for those seeking solace in the wilderness. Amidst the urban jungle, a graffiti artist painted murals that told stories of rebellion and self-expression. In the depths of the enchanted forest, fireflies danced in a mesmerizing display of bioluminescence.
 
@@ -33,13 +30,17 @@ Amidst the ancient ruins, a solitary flower bloomed, a testament to the enduring
 
 A gentle rain fell, cleansing the world of its worries and leaving a sense of calm in its wake. Within the castle's grand ballroom, couples twirled in an elegant dance, their laughter a symphony of joy. Across the endless desert, a caravan journeyed, their camels' footsteps etching stories into the shifting sands. The aroma of spices filled the air, a tantalizing invitation to explore the exotic flavors of a foreign market. In the depths of the ocean, a coral reef teemed with vibrant life, a hidden oasis beneath the waves.
 
-Beneath a canopy of cherry blossoms, a poet penned verses that captured the ephemeral beauty of the moment. Through the ancient forest, a stream murmured secrets to the stones, its babbling a soothing lullaby. A lone wolf howled at the moon, its mournful cry a reminder of the wild's untamed spirit. In the heart of a snow-covered village, children built snowmen with boundless enthusiasm, their laughter infectious. The clock tower's chimes marked the passage of time, a rhythmic pulse that echoed through the bustling city streets.
-
-Amidst the rolling vineyards, that you can use as a starting point for your book. Feel free to modify and expand upon them to suit the themes and narrative you have in mind.`
+Beneath a canopy of cherry blossoms, a poet penned verses that captured   in mind.`
 
 
 
-const bookContent = {paraOne, paraTwo, paraThree, paraFour};
+const paraFive = `The ephemeral beauty of the moment. Through the ancient forest, a stream murmured secrets to the stones, its babbling a soothing lullaby. A lone wolf howled at the moon, its mournful cry a reminder of the wild's untamed spirit. In the heart of a snow-covered village, children built snowmen with boundless enthusiasm, their laughter infectious. The clock tower's chimes marked the passage of time, a rhythmic pulse that echoed through the bustling city streets.
+
+Amidst the rolling vineyards, that you can use as a starting point for your book  . Feel free to modify and expand upon them to suit the themes and narrative you have. the depths of the crystal cave, a rare gemstone shimmered, its beauty a reflection of the earth's secrets a riddle awaited, promising untold riches to those who could decipher its code. Through the twisted alleyways, a rogue navigated with practiced ease, his cloak billowing behind him like a shadowy specter. The scent of rain hung heavy in the air, a promise of renewal and growth. At the crossroads of destiny, two souls collided, setting in motion a chain of events that would shape their fates forever.
+
+Within . Among the ruins of an ancient civilization, a lone archaeologist pieced together the puzzle of a lost civilization. The wind whispered ancient songs to the weary wanderer,. High atop the cliffs, a lighthouse stood sentinel, its beacon guiding lost souls home `
+
+const bookContent = {paraOne, paraTwo, paraThree, paraFour, paraFive};
 
 export default bookContent;
          
