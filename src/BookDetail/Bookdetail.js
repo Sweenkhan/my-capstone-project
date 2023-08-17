0714993164dad830c7039b4ef41550defa843f09
sween <khansween@gmail.com>
2023-08-17 20:59:20 +0530

    add new route in front-end

--- src/BookDetail/Bookdetail.js
+++ src/BookDetail/Bookdetail.js
@@ -1,35 +1,109 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useEffect } from "react";
+import { useContext, useState } from "react";
 import { searchedContext } from "../App/App";
 import bookContent from "./BookContent";
 import "./Bookdetail.css";
+import axios from "axios";
 // import book from '../../../back/models/book'
 
 function Bookdetail() {
 
-  const { bookDetail} = useContext(searchedContext);
-  const {paraOne, paraTwo, paraThree, paraFour} = bookContent;
-   
+  const { bookDetail } = useContext(searchedContext);
+  const { paraOne, paraTwo, paraThree, paraFour, paraFive } = bookContent;
+
+  const [rating, setRating] = useState(0);
+  const [clickRating, setClickRating] = useState(false)
+  const session = localStorage.getItem("session");
+ 
+  let ratingBook = bookDetail._id; 
+
+
+
+  function handleRate(stars) {
+    setRating(stars);
+    setClickRating(true) 
+  };
+
+
+//---------------------------COMPLETED BOOK------------------------
+  function completeBook(e){
+    let completedBook = bookDetail._id
+             e.preventDefault();
+       axios.patch("http://localhost:8080/completed", {completedBook, session})
+          .then((result) =>{
+              // if(result.status === 200){
+              //   console.log(result.data)
+              // }else{
+              //   console.log(result.data)
+              // }
+
+              console.log(result.data)
+          }).catch((err) =>{
+            console.log(err)
+         })
+  }
+ 
+
+  //----------------------------------RATING TO BOOKS--------------------
+  useEffect(() =>{
+       
+    if(clickRating){ 
+    axios.patch('http://localhost:8080/rating',{ratingBook, rating, session})
+    .then((result) =>{
+        if(result.status === 200){
+          console.log(result.data)
+        }
+    }).catch((err) =>{
+       console.log(err)
+    })
+  }
+
+  console.log(rating); 
+  },[rating])
+
+
+
 
   return (
-    <div className="bookdetail">
-      <div className="bookdetailCont"  >
-
+    <div className="bookdetail"> 
+      <div className="bookdetailCont">
         <div className="detail-left">
-          <img src={bookDetail.image} alt="searchBooks" />
+
+          <div className="detail-left-img">
+            <img src={bookDetail.image} alt="searchBooks" />
+          </div>
+
+          <div className="rating-section">
+            <p>Rate this book:</p>
+            <div className="stars">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <span
+                  key={star}
+                  className={`star ${rating >= star ? "active" : ""}`}
+                  onClick={() => handleRate(star)} >
+                  &#9733;
+                </span>
+              ))}
+            </div>
+            <p>{rating > 0 ? `Thank you for rating ${rating} stars!` : ""}</p>
+          </div>
         </div>
+
         <div className="detail-right">
           <h2>{bookDetail.title}</h2>
           <p>{bookDetail.author}</p>
           <div className="paragraph">
-        <h4 style={{marginLeft: "10px"}}>DESCRIPTION:</h4>   
-        <p className="description"><span>A. </span>{bookDetail.description} </p><hr /> 
-        <p><span>1. </span>{paraOne}</p><hr />
-        <p style={{backgroundColor: "white"}}><span>2. </span>{paraTwo}</p><hr />
-        <p><span>3. </span>{paraThree}</p><hr />
-        <p style={{backgroundColor: "white"}}><span>4. </span>{paraFour}</p><hr />
+            <h4 style={{ marginLeft: "10px" }}>DESCRIPTION:</h4>
+            <p className="description"> <span>A. </span> {bookDetail.description}{" "} </p> <hr />
+            <p> <span>1. </span>  {paraOne}  </p> <hr />
+            <p style={{ backgroundColor: "white" }}> <span>2. </span> {paraTwo}  </p>  <hr />
+            <p> <span>3. </span> {paraThree} </p> <hr />
+            <p style={{ backgroundColor: "white" }}><span>4. </span> {paraFour}</p> <hr />
+            <p> <span>5. </span> {paraFive} </p> <hr />
+            <div><button onClick={(e) => {completeBook(e, )}}>COMPLETE </button></div>
+          </div>
         </div>
-        </div>
+
       </div>
     </div>
   );
