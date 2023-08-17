1b2b03cade17c85db7a7a56f0d215a9a74a383c1
sween <khansween@gmail.com>
2023-08-17 08:03:05 +0530

    commit

---
+++ src/BookDetail/BookContent.js
@@ -0,0 +1,45 @@
+const paraOne = `In the distant land of Veridonia,lighthouse stood sentinel, its beacon guiding lost souls home.  In the distant land of Veridonia, where the sun kissed the emerald valleys, a tale of magic and mystery unfurled. The ancient oak trees whispered secrets to the wind, their leaves dancing in a graceful waltz. As the moon cast its silvery glow upon the tranquil waters, a lone figure emerged from the shadows. The scent of wildflowers hung in the air, weaving a tapestry of fragrant dreams. Amidst the bustling market, vendors peddled their wares
+
+with a chorus of voices that where the sun kissed the emerald valleys, a tale of magic and mystery unfurled. The ancient oak trees whispered secrets to the wind, their leaves dancing in a graceful waltz. As the moon cast its silvery glow upon the tranquil waters, a lone figure emerged from the shadows. The scent of wildflowers hung in the air, weaving a tapestry of fragrant dreams. Amidst the bustling market, vendors peddled their wares with a chorus of voices that echoed through the cobbled streets.
+
+High atop the cliffs, a lighthouse stood sentinel, its beacon guiding lost souls home.  In the distant land of Veridonia, where the sun kissed the emerald valleys, a tale of magic and mystery unfurled. The ancient oak trees whispered secrets to the wind, their leaves dancing in a graceful waltz. As the moon cast its silvery glow upon the tranquil waters, a lone figure emerged from the shadows. The scent of wildflowers hung in the air, weaving a tapestry of fragrant dreams. Amidst the bustling market, vendors peddled their wares
+
+he cobbled streets. High atop the cliffs, a lighthouse stood sentinel, its beacon guiding lost souls home,
+
+ 
+ `;
+
+const paraThree = `Through the pages of time, a forgotten legend began to resurface, like ink spreading through parchment. Beneath the star-studded canopy, a campfire crackled, casting dancing shadows upon the faces of weary travelers. The castle walls stood tall and proud, a testament to the strength and resilience of a bygone era. In the heart of the enchanted forest, magical creatures frolicked in harmony, their laughter a symphony of joy. Across the rolling hills, a river meandered, its waters teeming with secrets yet to be discovered.
+
+A lone ship sailed toward the horizon, its sails billowing like the wings of a majestic bird. With each brushstroke, the artist breathed life into the canvas, creating a masterpiece that would endure through time. In the quiet village, children chased butterflies, their laughter a melody that carried on the breeze. The aroma of freshly baked bread wafted from the bakery, tempting even the most disciplined of souls. Hidden deep within the labyrinthine catacombs, an ancient artifact lay dormant, waiting for the chosen one to awaken its power.
+
+As the sun dipped below the horizon, a symphony of colors painted the sky, leaving all who beheld it in awe. Among the pages of an old tome, a riddle awaited, promising untold riches to those who could decipher its code. Through the twisted alleyways, a rogue navigated with practiced ease, his cloak billowing behind him like a shadowy specter. The scent of rain hung heavy in the air, a promise of renewal and growth. At the crossroads of destiny, two souls collided, setting in motion a chain of events that would shape their fates forever.
+
+Within the depths of the crystal cave, a rare gemstone shimmered, its beauty a reflection of the earth's secrets. Among the ruins of an ancient civilization, a lone archaeologist pieced together the puzzle of a lost civilization. The wind whispered ancient songs to the weary wanderer, carrying tales of far-off lands and forgotten heroes. In the heart of the bustling city, a street performer captivated a crowd with his mesmerizing dance. The tapestries adorning the palace walls told stories of battles won, alliances forged, and love that transcended time.
+` 
+
+const paraFour = ` a vintner toiled, crafting wines that would tell stories of sun-soaked fields and rich soil. The flickering candlelight cast eerie shadows upon the ancient manuscripts, where forgotten knowledge lay dormant. Within the opulent palace, a masquerade ball unfolded, the guests' elaborate masks concealing their true identities. Across the celestial expanse, shooting stars streaked, carrying wishes to distant corners of the universe. In the heart of a bustling kitchen, a chef orchestrated a symphony of flavors, each dish a work of culinary art.
+
+The ancient standing stones whispered secrets to those who listened, their enigmatic messages a puzzle to be deciphered. A nomadic tribe traversed the endless dunes, their camels carrying tales of a life lived in harmony with the desert. The scent of pine and woodsmoke hung in the air, a comforting embrace for those seeking solace in the wilderness. Amidst the urban jungle, a graffiti artist painted murals that told stories of rebellion and self-expression. In the depths of the enchanted forest, fireflies danced in a mesmerizing display of bioluminescence.
+
+The pages of a forgotten diary held the musings of a bygone era, a glimpse into lives once lived. Beneath the starry sky, a bonfire blazed, its crackling embers carrying wishes to the cosmos. Among the towering cliffs, a waterfall cascaded, its crystalline waters a source of life and renewal. The aroma of incense filled the temple, its fragrant tendrils reaching toward the heavens in silent supplication. In the heart of the bustling harbor, fishermen mended their nets, their weathered hands a testament to their enduring connection with the sea."
+
+These are just a few lines of dummy text`
+
+
+const paraTwo = `As the seasons shifted, the forest transformed, each hue and shade a brushstroke in nature's ever-changing masterpiece. Beneath the twinkling stars, a campfire's glow warmed the hearts of travelers as they shared tales of their adventures. The scent of pine needles mingled with the salty breeze, a reminder of the rugged beauty of the coastal cliffs. Through the telescope's lens, distant galaxies unveiled their mysteries, beckoning explorers to venture beyond the stars. In the heart of the city, skyscrapers reached for the heavens, their glass facades reflecting a modern world in constant motion.
+
+Amidst the ancient ruins, a solitary flower bloomed, a testament to the enduring spirit of life. The sound of thunder echoed across the plains, a herald of the storm's approach and the chaos it would bring. The melody of a lone violin filled the air, its haunting notes carrying the weight of a thousand sorrows. Among the bustling market stalls, a fortune teller gazed into her crystal ball, unraveling the threads of fate. Through the mist-shrouded forest, a path led to a forgotten temple, where whispers of ancient rituals lingered.
+
+A gentle rain fell, cleansing the world of its worries and leaving a sense of calm in its wake. Within the castle's grand ballroom, couples twirled in an elegant dance, their laughter a symphony of joy. Across the endless desert, a caravan journeyed, their camels' footsteps etching stories into the shifting sands. The aroma of spices filled the air, a tantalizing invitation to explore the exotic flavors of a foreign market. In the depths of the ocean, a coral reef teemed with vibrant life, a hidden oasis beneath the waves.
+
+Beneath a canopy of cherry blossoms, a poet penned verses that captured the ephemeral beauty of the moment. Through the ancient forest, a stream murmured secrets to the stones, its babbling a soothing lullaby. A lone wolf howled at the moon, its mournful cry a reminder of the wild's untamed spirit. In the heart of a snow-covered village, children built snowmen with boundless enthusiasm, their laughter infectious. The clock tower's chimes marked the passage of time, a rhythmic pulse that echoed through the bustling city streets.
+
+Amidst the rolling vineyards, that you can use as a starting point for your book. Feel free to modify and expand upon them to suit the themes and narrative you have in mind.`
+
+
+
+const bookContent = {paraOne, paraTwo, paraThree, paraFour};
+
+export default bookContent;
+         
--- src/BookDetail/Bookdetail.js
+++ src/BookDetail/Bookdetail.js
@@ -1,47 +1,38 @@
- 
-import React from 'react'
-import { useContext } from 'react'
-import { searchedContext } from '../App/App'
-import "./Bookdetail.css"
+import React from "react";
+import { useContext } from "react";
+import { searchedContext } from "../App/App";
+import bookContent from "./BookContent";
+import "./Bookdetail.css";
 // import book from '../../../back/models/book'
-
 
 function Bookdetail() {
 
-    const {bookDetail, currentRead } = useContext(searchedContext);
-    
+  const { bookDetail} = useContext(searchedContext);
+  const {paraOne, paraTwo, paraThree, paraFour} = bookContent;
    
-    function colorGenrate(){ 
-        const letter = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d", "e", "f"]; 
-          let generate = "#";
-       
-       for(let i = 0; i < 6; i++){
-           generate += letter[Math.floor(Math.random() * letter.length)]
-       } 
-       return  generate += 60;
-    } 
-  let generate = colorGenrate() 
 
-  console.log(currentRead);
-
-  
- 
   return (
-    <div className='bookdetail'> 
-        <div className='row-img'  style={{background: `linear-gradient(${generate}, rgba(0, 0, 0, 0))`}}>
-              <div className='searced-img'>
-              <img src={bookDetail.image} alt="searchBooks" />
-              </div>
-              <div className='searchedCnt'>
-              <h2>{bookDetail.title}</h2>
-                <p>{bookDetail.author}</p>
-              </div> 
-            </div>
-                <p className='description'>{bookDetail.description}</p> 
-            </div>
-  )
+    <div className="bookdetail">
+      <div className="bookdetailCont"  >
+
+        <div className="detail-left">
+          <img src={bookDetail.image} alt="searchBooks" />
+        </div>
+        <div className="detail-right">
+          <h2>{bookDetail.title}</h2>
+          <p>{bookDetail.author}</p>
+          <div className="paragraph">
+        <h4 style={{marginLeft: "10px"}}>DESCRIPTION:</h4>   
+        <p className="description"><span>A. </span>{bookDetail.description} </p><hr /> 
+        <p><span>1. </span>{paraOne}</p><hr />
+        <p style={{backgroundColor: "white"}}><span>2. </span>{paraTwo}</p><hr />
+        <p><span>3. </span>{paraThree}</p><hr />
+        <p style={{backgroundColor: "white"}}><span>4. </span>{paraFour}</p><hr />
+        </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-export default Bookdetail
+export default Bookdetail;
