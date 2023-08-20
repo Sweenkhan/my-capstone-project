21fa8f82700b7d7adcc26af6150988a308660427
sween <khansween@gmail.com>
2023-08-21 03:14:14 +0530

    removed userhasloggedin context variable

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,20 +5,24 @@ import SearchIcon from '@mui/icons-material/Search';
 import axios from 'axios';
 import { useContext } from 'react';
 import { searchedContext } from '../App/App';
-import { useNavigate } from 'react-router-dom';
-import { userContext } from '../App/App';
-
+import { useNavigate } from 'react-router-dom';  
 
 function Header() {
 
-  const {setSearchedBooks} = useContext(searchedContext)
-  const {hasUserLoggedin, setHasUserLoggedin} = useContext(userContext)
+  const {setSearchedBooks} = useContext(searchedContext); 
 
-
+  const [hasfriend, setHasFriend] = useState(false)
+  const [allUsers, setAllUsers] = useState([])
   const [searchBook, setSearchBook] = useState("");
   const navigate = useNavigate()
+  const session = localStorage.getItem("session")
 
-  
+  const headers = {
+    "Content-Type": "application/json",
+    authorization: session,
+  };
+
+//----------------------------GET SEARCH BOOKS----------------------------//
   function handleGsearch(e) { 
     e.preventDefault()
       axios.post("http://localhost:8080/search", {searchBook})
@@ -37,17 +41,37 @@ function Header() {
     
   }
 
-
+//----------------------------HANDLE LOGOUT REQUEST------------------------------//
   function handlLogout(e){
          e.preventDefault()
           
-          localStorage.setItem("session", "")
-          setHasUserLoggedin(false)
+          localStorage.setItem("session", "") 
           navigate("/")
           console.log("user has logged out")
-         
   }
 
+//-------------------------GET ALL USERS-----------------------------------------//
+  function getUsers(e){
+        e.preventDefault()
+        console.log(session)
+        axios.get("http://localhost:8080/allusers", {headers})
+           .then((result)=>{
+            setAllUsers(result.data)
+            console.log(result.data)
+            setHasFriend(true)
+           })
+  }
+
+
+//--------------------SENDING FRIEND REQUES------------------------------------//
+
+function sendRequest(e, friendUsername){
+   e.preventDefault(); 
+   axios.patch("http://localhost:8080/frienRequest", {session, friendUsername})
+     .then((result) => {
+         console.log(result.data)
+     })
+}
 
 
   return (
@@ -70,10 +94,17 @@ function Header() {
              <Link to="/book">Books</Link>
            </li>
            <li>{
-            (hasUserLoggedin) ? <button onClick={handlLogout}>Logout</button> : <Link id='login' to="/login">Login</Link>
+            (localStorage.getItem("session")) ? <button onClick={handlLogout}>Logout</button> : <Link id='login' to="/login">Login</Link>
            }
-              
            </li>
+           <li onClick={getUsers} className='friends'>Friends </li>
+            {
+              (hasfriend && localStorage.getItem("session")) ? <ul className='drop-down-friendlist'><li onClick={e => setHasFriend(false)}>X</li>{
+              allUsers.map((usr, index) => {
+              return <li key={index} >{usr.username}<span style={{backgroundColor: "red", fontSize: "8px"}} onClick={e => sendRequest(e, usr.username)}> sending</span></li>
+            })}
+            </ul>: ""}
+             
             
         </ul>
         
