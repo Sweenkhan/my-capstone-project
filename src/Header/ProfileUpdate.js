7db0ff76ff130e707d86dc7d2ac2297e251892f8
sween <khansween@gmail.com>
2023-10-13 23:49:44 +0530

    added functionality to user can update prfile

--- src/Header/ProfileUpdate.js
+++ src/Header/ProfileUpdate.js
@@ -1,32 +1,82 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios';
+import { porturl } from '../url/porturl';
+import { toast } from 'react-toastify';
+
 
 function ProfileUpdate(props) {
 
+    const prop = props.userProfile;
+    const session = localStorage.getItem("session")
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [edit, setEdit] = useState(false)
+
+    const [username, setUserName] = useState(prop.username)
+    const [name, setName] = useState(prop.name)
+    const [email, setEmail] = useState(prop.email)
+    const [phone, setPhone] = useState(prop.phone)
+    const [password, setPassword] = useState(prop.originalPassword)
+
+ 
+//   const [name, setName] = useState("")
+//   const [email, setEmail] = useState("")
+//   const [phone, setPhone] = useState("")
+//   const [username, setUsername] = useState("")
+//   const [password, setPassword] = useState("")
+    
+    console.log(edit)
+    function handleSubmit(e){ 
+         e.preventDefault()
+
+      if(edit){
+        axios.put(`${porturl}/editUserProfile/${session}`, {name, email, phone, username, password})
+        .then((result) => {
+            toast.success("Profile update successfully!")
+           console.log(result.data.message)
+        }).catch((err) => {
+           console.log(err)
+        })
+      } else {
+        toast.info("nothing has changed")
+      }
+
+    }
+    
+
   return (
     <div className='updateProfile'>
-    <h2>this is profile page </h2>
-         <form>
-            <label for="username">Username: </label>
-            <input value={props.username} type='text' id='username'/>
+    <h2>USER PROFILE</h2>
+         <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username: </label>
+            <input value={username} onChange={(e) => setUserName(e.target.value)}
+             type='text' id='username' disabled />
 
-            <label for="name">Name </label>
-            <input value={props.name} type='text' id='name'/>
+            <label htmlFor="name">Name </label>
+            <input value={name} onChange={(e) => setName(e.target.value)} type='text' 
+            id='name' disabled={(!edit) ? true : false} />
 
-            <label for="mobile">Phone </label>
-            <input value={props.phone} type='number' id='mobile'/>
+            <label htmlFor="mobile">Phone </label>
+            <input value={phone} onChange={(e) => setPhone(e.target.value)} type='number'
+             id='mobile' disabled={(!edit) ? true : false} />
 
-            <label for="email">Email </label>
-            <input value={props.email} type='email' id='email' readOnly/>
+            <label htmlFor="email">Email </label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type='email'
+             id='email' disabled />
             
-            <label for="password">Password: </label>
-            <input value={props.password} type='password' id='password' readOnly/>
+            <label htmlFor="password" className='password'>Password:  
+            <span className="showPassword" onClick={() => {setShowPassword((!showPassword) ? true : false)}}>Show</span></label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type={(showPassword) ? "text" : "password" } 
+            id='password' disabled={(!edit) ? true : false} />
 
             <button type='submit'>Save</button>
         </form>
+
         <div className='buttons'> 
-        <button>Edit</button>
+        <button onClick={()=> {setEdit((!edit) ? true : false)}}>Edit</button>
         <button onClick={() => props.closing(false)}>Close</button>
         </div>
+
     </div>
   )
 }
