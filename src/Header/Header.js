a4d9676d5d42dbd843dcf92dca905bcdb8cec71f
sween <khansween@gmail.com>
2023-10-25 17:23:05 +0530

    worked in menu

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,36 +8,31 @@ import { searchedContext } from "../App/App";
 import { useNavigate } from "react-router-dom";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import UserProfile from "./UserProfile";
-import { porturl } from "../url/porturl";  
-import MenuIcon from '@mui/icons-material/Menu';
-
+import { porturl } from "../url/porturl";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Header() {
-  const { setSearchedBooks} = useContext(searchedContext);
- 
-    // const [allUsers, setAllUsers] = useState([])
+  const { setSearchedBooks } = useContext(searchedContext);
+
+  // const [allUsers, setAllUsers] = useState([])
   const [showUserProfile, setShowUserProfile] = useState(false);
   const [searchBook, setSearchBook] = useState("");
-  const [bookType, setBookType] = useState("")
-  const [showMenuIcon, setShowMenuIcon] = useState(false)
-  
-  const menuIcon  = window.screen.width < 500 
-  // console.log(menuIcon)
-   
+  const [bookType, setBookType] = useState("");
+  const [showMenuIcon, setShowMenuIcon] = useState(false);
+  const [menuIcon, setMenuIcon] = useState(false);
+
   const navigate = useNavigate();
   const session = localStorage.getItem("session");
-
 
   //----------------------------GET SEARCH BOOKS----------------------------//
   function handleGsearch(e) {
     e.preventDefault();
     axios
       .post(porturl + "/search", { searchBook })
-      .then((result) => {  
-
-        setSearchedBooks(result.data.results); 
-        if(result.data.results.length === 0){
-          alert("Sorry didn't match with any title.  Try Again ")
+      .then((result) => {
+        setSearchedBooks(result.data.results);
+        if (result.data.results.length === 0) {
+          alert("Sorry didn't match with any title.  Try Again ");
         } else {
           navigate("/search");
         }
@@ -48,97 +43,153 @@ function Header() {
       });
   }
 
-
   //------------------------------GET SEARCH BY BOOKTYPE------------------------------------//
 
-  useEffect(() =>{
-    console.log(bookType) 
-    axios.post(porturl + "/searchBookByType", {bookType })
-    .then((result) => {  
+  useEffect(() => {
+    console.log(bookType);
+    axios
+      .post(porturl + "/searchBookByType", { bookType })
+      .then((result) => {
+        // eslint-disable-next-line
+        setSearchedBooks(result.data.results);
+        if (result.data.results.length === 0) {
+          console.log("not found");
+        } else {
+          // eslint-disable-next-line
+          navigate("/search");
+        }
+        // eslint-disable-next-line
+        setSearchBook("");
+      })
+      .catch((err) => {
+        console.log("error to bta do", err);
+      });
+  }, [bookType]);
 
-      setSearchedBooks(result.data.results); 
-      if(result.data.results.length === 0){ 
-        console.log("not found")
-      } else {
-        navigate("/search"); 
-      }
-      setSearchBook("");
-    })
-    .catch((err) => {
-      console.log("error to bta do", err);
-    });
-
-  },[bookType])
-    
-
-  console.log(showMenuIcon)
-  return (
  
-    <div className="header">
-    <div className="combine"> 
-      <div className="main-logo">
-        <Link to="/">
-          {" "}
-          <h2 className="text-light">
-            BOOK<span style={{ color: "black" }}>SHELF </span>
-          </h2>{" "}
-        </Link>
-      </div>
-        
-      <form 
-        onSubmit={handleGsearch} >
-        <select className="searchByType" onChange={e => setBookType(e.target.value)} defaultValue="placeholder"> 
-        <option value="placeholder"  disabled ></option>
-        <option disabled>Type</option>
-        <option value="poetry">Poetry</option>
-        <option value="flower">Flower</option>
-        <option value="romance">Romance</option>
-        <option value="fantasy">Fantasy</option>
-        <option value="mostpopular">Popular</option>
-        </select>
 
-        <input
-          type="search"
-          id="search"
-          name="gsearch"
-          value={searchBook}
-          placeholder="Find books..."
-          onChange={(e) => {
-            setSearchBook(e.target.value);
-          }}
-          
-        />
-        <button className="searchIcon"> 
-          <SearchIcon sx={{ fontSize: 32 }}/>
+  //----------------------------function for check window innderwidth --------------------------
+    function handleResize() {
+      if (window.innerWidth < 500) {
+        setMenuIcon(true);
+      } else {
+        setMenuIcon(false);
+      }
+      console.log("hiii")
+    }
+ 
+    window.addEventListener("resize", handleResize);            //adding event listener to window
+ 
+
+  return (
+    <div className="header">
+      <div className="combine">
+        <div className="main-logo">
+          <Link to="/">
+            {" "}
+            <h2 className="text-light">
+              BOOK<span style={{ color: "black" }}>SHELF </span>
+            </h2>{" "}
+          </Link>
+        </div>
+
+        <form onSubmit={handleGsearch}>
+          <select
+            className="searchByType"
+            onChange={(e) => setBookType(e.target.value)}
+            defaultValue="placeholder"
+          >
+            <option value="placeholder" disabled></option>
+            <option disabled>Type</option>
+            <option value="poetry">Poetry</option>
+            <option value="flower">Flower</option>
+            <option value="romance">Romance</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="mostpopular">Popular</option>
+          </select>
+
+          <input
+            type="search"
+            id="search"
+            name="gsearch"
+            value={searchBook}
+            placeholder="Find books..."
+            onChange={(e) => {
+              setSearchBook(e.target.value);
+            }}
+          />
+          <button className="searchIcon">
+            <SearchIcon sx={{ fontSize: 32 }} />
           </button>
-      </form>
+        </form>
       </div>
-{( menuIcon) && <span><MenuIcon sx={{ fontSize: 44 }} className="menuIcon" onClick={() => {(!showMenuIcon) ? setShowMenuIcon(true) : setShowMenuIcon(false) }} /></span>}
-      <ul style={{top: (showMenuIcon) ? "94px" : "-150px" }}>
+      {menuIcon && (
+        <span>
+          <MenuIcon
+            sx={{ fontSize: 44 }}
+            className="menuIcon"
+            onClick={() => {
+              !showMenuIcon ? setShowMenuIcon(true) : setShowMenuIcon(false);
+            }}
+          />
+        </span>
+      )}
+      <ul style={{ top: showMenuIcon ? "94px" : "-150px" }}>
         <li>
-          <Link to="/about" onClick={() =>{menuIcon && setShowMenuIcon(false)}} > About </Link>
+          <Link
+            to="/about"
+            onClick={() => {
+              menuIcon && setShowMenuIcon(false);
+            }}
+          >
+            {" "}
+            About{" "}
+          </Link>
         </li>
         <li>
-          <Link to="/dashboard" onClick={() =>{menuIcon && setShowMenuIcon(false)}}>Dashboard</Link>
+          <Link
+            to="/dashboard"
+            onClick={() => {
+              menuIcon && setShowMenuIcon(false);
+            }}
+          >
+            Dashboard
+          </Link>
         </li>
         <li>
-          <Link to="/book" onClick={() =>{menuIcon && setShowMenuIcon(false)}}>Books</Link>
+          <Link
+            to="/book"
+            onClick={() => {
+              menuIcon && setShowMenuIcon(false);
+            }}
+          >
+            Books
+          </Link>
         </li>
         <li>
           {session ? (
             <span
-            className="main-user"
+              className="main-user"
               onClick={(e) => {
-                (showUserProfile)
+                showUserProfile
                   ? setShowUserProfile(false)
                   : setShowUserProfile(true);
               }}
             >
               {" "}
-              <AccountCircleIcon sx={{ fontSize: 40 }} className="text-light logo" />
+              <AccountCircleIcon
+                sx={{ fontSize: 40 }}
+                className="text-light logo"
+              />
             </span>
           ) : (
-            <Link id="login" to="/login" onClick={() =>{menuIcon && setShowMenuIcon(false)}} >
+            <Link
+              id="login"
+              to="/login"
+              onClick={() => {
+                menuIcon && setShowMenuIcon(false);
+              }}
+            >
               Login
             </Link>
           )}
@@ -152,9 +203,8 @@ function Header() {
             ""
           )}
         </li>
-      </ul> 
+      </ul>
     </div>
-    
   );
 }
 
