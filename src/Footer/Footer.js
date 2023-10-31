be7d0804b564a97df17b4f0278912abb75378f8f
sween <khansween@gmail.com>
2023-10-31 17:41:01 +0530

    added

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -11,12 +11,10 @@ function Footer() {
     <div className='footer'>
     <div className='container'>
     <div className='contact'>
-        <h4>touch-us</h4>
-    <p> vikas.tomar@gmail.com </p>
-        {/* <p>khansween@gmail.com</p> */}
+        <h4>touch-us</h4> 
+        <p>khansween@gmail.com</p>
     </div>
-    <p ><span>© BOOKSHELF</span> All rights reserved. </p>
-    {/* <p>Created by Sween Khan </p> */}
+    <p ><span>© BOOKSHELF</span> All rights reserved. Created by Sween Khan </p>
     <ul>
         <li>
             <Link to="https://www.instagram.com"><InstagramIcon sx={{ fontSize: 40 }} /></Link>
